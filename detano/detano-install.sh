662d48e11ceac844d83cba0659f5f76ef1b98d32
vhavlena <vohavlena@gmail.com>
2021-10-19 13:22:11 +0200

    automata-based detection: install

---
+++ detano/detano-install.sh
@@ -0,0 +1,8 @@
+
+#Install system packages
+sudo apt-get update
+sudo apt install python3-pip
+
+#Install python packages
+git clone https://github.com/vhavlena/detano.git
+pip3 install -r requirements.txt

902bf8bb3a43d3ddf235a408dfd8f0dd5693589f
Ondrej Rysavy <rysavy@fit.vutbr.cz>
2022-01-06 11:26:21 +0100

    Add new project for anomaly injection to datasets.

---
+++ imadics/source/AnomalyInjector/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AnomalyInjector
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

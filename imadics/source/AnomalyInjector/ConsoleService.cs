711534956931e4046ab96b0f859e5617cdbf72e7
Ondrej Rysavy <rysavy@fit.vutbr.cz>
2022-01-18 12:07:31 +0100

    Add command.

---
+++ imadics/source/AnomalyInjector/ConsoleService.cs
@@ -0,0 +1,141 @@
+﻿using Microsoft.Extensions.CommandLineUtils;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AnomalyInjector
+{
+    internal class ConsoleService : IHostedService
+    {
+        private readonly ILogger _logger;
+        private readonly IHostApplicationLifetime _appLifetime;
+        private Task _runningTask;
+        private CommandArgument _modbusServerEndpointArgument;
+
+        public ConsoleService(
+            ILogger<ConsoleService> logger,
+            IHostApplicationLifetime appLifetime)
+        {
+            _logger = logger;
+            _appLifetime = appLifetime;
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            _runningTask = Task.Factory.StartNew(OnRun, cancellationToken);
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            return _runningTask;
+        }
+
+        internal IPEndPoint ServerEndPoint
+        {
+            get
+            {
+                var serverString = _modbusServerEndpointArgument.Value;
+                if (serverString == null) return null;
+                var epString = serverString.Split('/')[0];
+                IPEndPoint.TryParse(epString, out var ep);
+                return ep;
+            }
+        }
+
+        internal int DeviceId
+        {
+            get
+            {
+                var serverString = _modbusServerEndpointArgument.Value;
+                if (serverString == null) return 0;
+                var stringParts = serverString.Split('/');
+                if (stringParts.Length > 1)
+                {
+                    int.TryParse(stringParts[1], out var deviceId);
+                    return deviceId;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+        }
+        private void OnRun()
+        {
+            var args = Environment.GetCommandLineArgs().Skip(1).ToArray();
+            _logger.LogInformation($"Starting with arguments: {string.Join("__", args)}");
+
+            var app = new CommandLineApplication
+            {
+                Name = "Anomalify",
+                Description = "Anomalify is a simple application that when running can simulate various error and attack situations in the running Factory I/O scene.",
+            };
+
+            app.HelpOption("-?|-h|--help");
+            app.VersionOption("-v|--version", () =>
+            {
+                return string.Format("Version {0}", Assembly.GetEntryAssembly().GetCustomAttribute<AssemblyInformationalVersionAttribute>().InformationalVersion);
+            });
+
+            _modbusServerEndpointArgument = new CommandArgument() { Name = "server", Description = "Specifies MODBUS endpoint connection string, e.g., `192.168.111.17:502/1`." };
+            app.Command("Address-Scan", AddressScanCommand);
+
+            try
+            {
+
+                app.Execute(args);
+            }
+            catch (CommandParsingException ex)
+            {
+                _logger.LogError("Incorrect arguments {0}", ex.Message);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("Unable to execute application {0}", ex.Message);
+            }
+            _appLifetime.StopApplication();
+        }
+
+        private void AddressScanCommand(CommandLineApplication command)
+        {
+            command.Description = "Address scan reconnaissance stands for identification of MODBUS devices on the given IP address.";
+            command.HelpOption("-?|-h|--help");
+
+            command.Arguments.Add(_modbusServerEndpointArgument);
+            var scanRangeArgument = command.Argument("scanRange", "Defines the range of device addresses to scan.");
+
+            command.OnExecute(() =>
+            {
+                var range = GetListFromRange(scanRangeArgument.Value);
+                Console.WriteLine($"Address Scan {this.ServerEndPoint} : {String.Join(',', range)}");
+                return 0;
+            });
+        }
+
+        private int[] GetListFromRange(string value)
+        {
+            var parts = value.Split(',');
+            IEnumerable<int> GetPartRange(string p)
+            {
+                var r = p.Split("..");
+                if (r.Length == 2)
+                {
+                    var s = int.Parse(r[0]);
+                    var e = int.Parse(r[1]);
+                    return Enumerable.Range(s, 1 + e - s);
+                }
+                else
+                    return new int[] { int.Parse(r[0]) };
+            }
+
+            return parts.SelectMany(GetPartRange).ToArray();
+        }
+    }
+}
--- imadics/source/AnomalyInjector/Program.cs
+++ imadics/source/AnomalyInjector/Program.cs
@@ -1,12 +1,30 @@
-﻿using System;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
 
 namespace AnomalyInjector
 {
-    internal class Program
+    public sealed class Program
     {
-        static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            await Host.CreateDefaultBuilder(args)
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddHostedService<ConsoleService>();
+                })
+                .ConfigureLogging((hostContext, logging) =>
+                {
+                    logging.AddConsole(options =>
+                    {
+                        options.LogToStandardErrorThreshold = LogLevel.Information;
+                        options.IncludeScopes = true;
+                        options.TimestampFormat = "hh:mm:ss ";
+                    });
+                    logging.SetMinimumLevel(LogLevel.Information);
+                })
+                .RunConsoleAsync();
         }
     }
 }
